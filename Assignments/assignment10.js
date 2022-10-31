99c5b35b5dfd22dc91dd33c33d52649325503090
redo6340 <redo6340@colorado.edu>
2022-10-30 23:44:17 -0600

    update

--- Assignments/assignment10.js
+++ Assignments/assignment10.js
@@ -1,4 +1,4 @@
-{
+const employees = {
     "companyName" : "Tech Stars",
     "website" : "www.techstars.site",
     "employees" : [
@@ -8,7 +8,7 @@
         "designation" : "Manager",
         "salary" : 40000,
         "raiseEligible" : true
-        ]
+        
       },
       {
         "firstName" : "Mary",
@@ -16,7 +16,7 @@
         "designation" : "Trainee",
         "salary" : 18500,
         "raiseEligible" : true
-        ]
+        
       },
       {
         "firstName" : "Bill",
@@ -24,7 +24,7 @@
         "designation" : "Executive",
         "salary" : 21200,
         "raiseEligible" : false
-        ]
+        
       },
       {
         "firstName" : "Anna",
@@ -32,7 +32,33 @@
         "designation" : "Executive",
         "salary" : 25600,
         "raiseEligible" : false
-        ]
+        
       }
     ]
   }
+
+
+  function populateHeader(obj) {
+    let header = document.querySelector('header');
+    let myH1 = document.createElement('h1');
+    myH1.textContent = obj.companyName;
+    header.appendChild(myH1);
+
+    const myPara = document.createElement('p');
+    myPara.textContent = `Website: ${obj.website} // Department: ${obj.formed}`;
+    header.appendChild(myPara);
+  }
+
+  function populateHeroes(obj) {
+    const section = document.querySelector('section');
+    const employee = obj.employees;
+
+    for (const employee of employees) {
+      const myPara1 = document.createElement('p');
+      myPara1.textContent = `The total salaries of all employees is: ${employee.salary}`;
+      myArticle.appendChild(myPara1);
+      
+    }
+  }
+
+  update();
