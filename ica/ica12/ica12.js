8614e0a680bd5f35042aaf3e4b6dddfdad40c48f
redo6340 <redo6340@colorado.edu>
2022-11-08 10:02:42 -0700

    j

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,10 +1,52 @@
+const minusButton = document.querySelector('.minus-Button').addEventListener('click', check);
+const plusButton = document.querySelector('.plus-Button').addEventListener('click', check);
+const resetButton = document.querySelector('.reset-Button').addEventListener('click', reset);
+const submitButton = document.querySelector('.submit-Button').addEventListener('click', submit);
 
-const button = document.querySelector('.button');
 const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
 
-button.addEventListener('click', updateOutput);
-
-function updateOutput() {
-    output.textContent = phone_content.value;
+function check() {
+    console.log('test');
 }
+
+function submit() {
+    alert(output.textContent);
+
+}
+
+function reset() {
+
+    output.textContent = '0000000000';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const button = document.querySelector('.button');
+//const output = document.querySelector('.output');
+//let phone_content = document.querySelector('.phone');
+
+//button.addEventListener('click', updateOutput);
+
+//function updateOutput() {
+   // output.textContent = phone_content.value;
+    //make the alrt dialog box appear
+    //alert(phone_content.value);
+//}
+
+
+//<input class="phone" type="text" id="phone" name="phone" placeholder="Enter a phone number"></input>
