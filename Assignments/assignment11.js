4f21c92f66c8864bc045fc6ce6d133acdef3f396
redo6340 <redo6340@colorado.edu>
2022-11-20 21:17:33 -0700

    promise

--- Assignments/assignment11.js
+++ Assignments/assignment11.js
@@ -43,3 +43,4 @@ function update() {
   }
 //gets Quote on screen
 getQuote();
+
---
+++ ica/ica13/ica13.js
@@ -0,0 +1,29 @@
+
+const aliceTumbling = [
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
+  ];
+  
+  const aliceTiming = {
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
+  }
+  
+  const alice1 = document.querySelector("#alice1");
+  const alice2 = document.querySelector("#alice2");
+  const alice3 = document.querySelector("#alice3");
+  
+  const alice1AnimationObj = alice1.animate(aliceTumbling, aliceTiming);
+  const { finished: alice1Finished } = alice1AnimationObj;
+  
+
+  alice1Finished
+    .then(() => {
+      return alice2.animate(aliceTumbling, aliceTiming).finished;
+    })
+    .then(() => {
+      return alice3.animate(aliceTumbling, aliceTiming).finished;
+    })
+    .then(() => console.log('All animations complete!'));
+  
