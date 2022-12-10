233ddc14ed99902c476a34ff626f2a9c345d5ae6
redo6340 <redo6340@colorado.edu>
2022-12-09 20:59:16 -0700

    r

--- photos.js
+++ photos.js
@@ -1,30 +1,35 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
-
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
 
 
-const pix = ['snowy.jpg', `momndad.jpg`, `books.jpg`, `erinnme.jpg`, `jacques.jpg`];
-const alternative = {
-  'snowy.jpg' : 'My view',
-  'momndad.jpg' : 'Parents',
-  'books.jpg' : 'Books',
-  'erinnme.jpg' : 'Ski day',
-  'jacques.jpg' : 'Jacques'
+function check() {
+    console.log('test');
 }
 
 
-for (const pic of pix) {
-  const changePic = document.createElement('img');
-  changePic.setAttribute('src', `../img/${pic}`);
-  changePic.setAttribute('alt', alternative[pic]);
-  thumbBar.appendChild(changePic);
-  changePic.addEventListener('click', e => {
-    displayedImage.src = e.target.src;
-    displayedImage.alt = e.target.alt;
-  });
+
+
+
+
+
+
+function random() {
+    outputInt = randomNumber(0, 8000000000);
+    alert(output.textContent);
+    output.textContent = outputInt;
 }
 
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+
+
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
 
 
